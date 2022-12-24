c284dab70a3050de2cacb2dc303943d60af292cc
fwho1150 <100674110+fwho1150@users.noreply.github.com>
2022-12-24 21:47:25 +0800

    Limit map width and height range

--- cmdargs.py
+++ cmdargs.py
@@ -3,24 +3,24 @@ import argparse
 parser = argparse.ArgumentParser()
 
 # common
-parser.add_argument('-mw', '--mapwidth', type=int, help='Map Width', 
-                    choices=range(5,20), metavar='[5-19]', 
+parser.add_argument('-mw', '--mapwidth', type=int, help='Map Width',
+                    choices=range(9, 20, 2), metavar='[9-19 odd]',
                     default=9)
 
-parser.add_argument('-mh', '--mapheight', type=int, help='Map Height', 
-                    choices=range(5,20), metavar='[5-19]', 
+parser.add_argument('-mh', '--mapheight', type=int, help='Map Height',
+                    choices=range(13, 20, 2), metavar='[13-19 odd]',
                     default=15)
 
-parser.add_argument('-d', '--difficulty', type=int, 
-                    help='Difficulty of the game, higher=more difficult', 
-                    choices=range(3), metavar='[0-2]', 
+parser.add_argument('-d', '--difficulty', type=int,
+                    help='Difficulty of the game, higher=more difficult',
+                    choices=range(3), metavar='[0-2]',
                     default=1)
 
-parser.add_argument('-s', "--seed", type=int, 
-                    help="The seed for random number generator", 
+parser.add_argument('-s', "--seed", type=int,
+                    help="The seed for random number generator",
                     default=None)
 
-parser.add_argument('-fps', "--fps", type=int, 
+parser.add_argument('-fps', "--fps", type=int,
                     help="The rendering speed in frames per second",
                     default=15)
 
@@ -29,15 +29,15 @@ parser.add_argument('-r', '--render',
                     help="Render game state to screen",
                     action="store_true")
 
-parser.add_argument('-e', "--episodes", type=int, 
-                    help="The number of episodes", 
+parser.add_argument('-e', "--episodes", type=int,
+                    help="The number of episodes",
                     default=1000)
 
-parser.add_argument('-ms', "--maxstep", type=int, 
-                    help="The maximum number of steps in an episode", 
+parser.add_argument('-ms', "--maxstep", type=int,
+                    help="The maximum number of steps in an episode",
                     default=500)
 
-parser.add_argument('-f', "--file", type=str, 
+parser.add_argument('-f', "--file", type=str,
                     help="The file name of the DQN model",
                     default=None)
 
